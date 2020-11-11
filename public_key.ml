292715171c556b8fed187052a10f23b375678ebd
Izaak Meckler <ihmeckler@gmail.com>
2020-11-11 08:09:19 -0800

    fix json encoding for public keys

--- public_key.ml
+++ public_key.ml
@@ -1,4 +1,27 @@
+open Core_kernel
+
+(* A point on the curve *)
+type t = Snarkette.Tweedle.Dee.t
+
 module Compressed = struct
   (* the x coordinate and y-parity of a curve point *)
-  type t = {x: Base_field.t; is_odd: bool} [@@deriving yojson]
+  module T = struct
+    type t = {x: Base_field.t; is_odd: bool} [@@deriving bin_io]
+  end
+
+  let version_byte = '\xCB'
+
+  let to_base58_check = Base58_check.to_base58_check ~version_byte (module T)
+
+  let of_base58_check = Base58_check.of_base58_check ~version_byte (module T)
+
+  include T
+
+  let to_yojson x = `String (to_base58_check x)
+
+  let of_yojson : Yojson.Safe.t -> _ = function
+    | `String s ->
+        Ok (of_base58_check s)
+    | _ ->
+        Error "bad public key"
 end
